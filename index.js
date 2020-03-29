058c155bc82b9a941892a72d0e1e9790ec3e72e2
Andrew Cherniavskii <andrew.cherniavskii@gmail.com>
2020-03-29 19:40:06 +0200

    refetch all queries on app focus

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const React = require('react');
+const { AppState } = require('react-native');
 const ReactQuery = require('react-query');
 const { useFocusEffect } = require('@react-navigation/native');
 
+// refetch all queries on app focus
+ReactQuery.setFocusHandler((handleFocus) => {
+  function  handler(state) {
+    if (state === 'active') {
+      handleFocus()
+    }
+  }
+  AppState.addEventListener('change', handler);
+
+  return () => {
+    AppState.removeEventListener('change', handler);
+  }
+})
 
 function useQuery(...args) {
   const query = ReactQuery.useQuery(...args);
